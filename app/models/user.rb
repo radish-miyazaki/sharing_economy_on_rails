0b3fd450d136e67e145d395bc31f39f9db8c4698
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-09-20 22:08:44 +0900

    :sparkles: Userモデルに新しいアトリビュート追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable,
          :lockable, :timeoutable, :trackable
+
+  enum gender: { unanswered: 0, make: 1, female: 2 }
+
+  validates :nickname, presence: true
+  validates :gender, presence: true, inclusion: { in: User.genders.keys }
+
+  class << self
+    def genders_i18n
+      I18n.t('enums.user.gender')
+    end
+  end
 end
