8c7d0e52bda48cba83f82116c55d03b18de16d21
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2022-10-10 00:01:16 +0900

    電話番号入力後に認証画面に飛ぶよう修正

--- app/controllers/user_mobile_phones_controller.rb
+++ app/controllers/user_mobile_phones_controller.rb
@@ -7,6 +7,14 @@ class UserMobilePhonesController < ApplicationController
 
   def create
     @user_mobile_phone = current_user.build_user_mobile_phone(user_mobile_phone_params)
+
+    if @user_mobile_phone.save
+      @user_mobile_phone.send_auth_code_to_sms
+      redirect_to verification_user_mobile_phone_path, notice: '電話番号登録に成功しました'
+    else
+      flash.now.alert = '電話番号登録に失敗しました'
+      render :new
+    end
   end
 
   def verification
